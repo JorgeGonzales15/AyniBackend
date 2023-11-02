6a3c49438e1110cdc8d8a130366475ca57ffb993
JorgeGonzales15 <u202118468@upc.edu.pe>
2023-11-02 07:57:00 -0500

    fix: update database hosting and fix table names

--- src/main/java/com/greatminds/ayni/shopping/domain/model/entities/Sale.java
+++ src/main/java/com/greatminds/ayni/shopping/domain/model/entities/Sale.java
@@ -1,12 +1,10 @@
 package com.greatminds.ayni.shopping.domain.model.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
+@Table(name="sales")
 public class Sale {
     @Id
     @Getter
