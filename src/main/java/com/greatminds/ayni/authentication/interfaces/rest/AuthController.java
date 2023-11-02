184c66c5adf5377b72485c67e24ef189e2e31316
Paolo Espejo <103698198+Loleao@users.noreply.github.com>
2023-11-02 03:47:01 -0500

    fix: Update authentication tag

--- src/main/java/com/greatminds/ayni/authentication/interfaces/rest/AuthController.java
+++ src/main/java/com/greatminds/ayni/authentication/interfaces/rest/AuthController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
+@Tag(name = "Authentication", description = "Authentication endpoints")
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
