368256e9dbc485bddb7af9df9cfd556bcc31c814
Caszarjos <zaratecastro2003@hotmail.com>
2023-11-17 00:44:48 -0500

    feat(authentication): Added Security config and Authentication Controller.

--- src/main/java/com/greatminds/ayni/authentication/interfaces/rest/AuthController.java
+++ src/main/java/com/greatminds/ayni/authentication/interfaces/rest/AuthController.java
@@ -1,49 +1,46 @@
 package com.greatminds.ayni.authentication.interfaces.rest;
 
-import com.greatminds.ayni.authentication.domain.model.payload.response.JwtResponse;
 import com.greatminds.ayni.authentication.domain.model.queries.GetUserByIdQuery;
 import com.greatminds.ayni.authentication.domain.services.UserCommandService;
 import com.greatminds.ayni.authentication.domain.services.UserQueryService;
-import com.greatminds.ayni.authentication.interfaces.rest.resources.AuthenticateUserResource;
-import com.greatminds.ayni.authentication.interfaces.rest.resources.CreateUserResource;
-import com.greatminds.ayni.authentication.interfaces.rest.transform.AuthenticateUserCommandFromResourceAssembler;
-import com.greatminds.ayni.authentication.interfaces.rest.transform.AuthenticationResourceFromAuthenticationResponseAssembler;
-import com.greatminds.ayni.authentication.interfaces.rest.transform.CreateUserCommandFromResourceAssembler;
+import com.greatminds.ayni.authentication.interfaces.rest.resources.SignInResource;
+import com.greatminds.ayni.authentication.interfaces.rest.resources.SignUpResource;
+import com.greatminds.ayni.authentication.interfaces.rest.transform.SignInCommandFromResourceAssembler;
+import com.greatminds.ayni.authentication.interfaces.rest.transform.AuthenticatedUserResourceFromEntityAssembler;
+import com.greatminds.ayni.authentication.interfaces.rest.transform.SignUpCommandFromResourceAssembler;
 import com.greatminds.ayni.authentication.interfaces.rest.transform.UserResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @Tag(name = "Authentication", description = "Authentication endpoints")
-@RequestMapping("/api/auth")
+@RequestMapping("/api/v1/auth")
 public class AuthController {
-    @Autowired
-    UserQueryService userQueryService;
-    @Autowired
-    UserCommandService userCommandService;
+
+    private final UserQueryService userQueryService;
+    private final UserCommandService userCommandService;
+
+    public AuthController(UserQueryService userQueryService, UserCommandService userCommandService) {
+        this.userQueryService = userQueryService;
+        this.userCommandService = userCommandService;
+    }
 
     @PostMapping("/signin")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody AuthenticateUserResource resource){
-        var authenticateUserCommand = AuthenticateUserCommandFromResourceAssembler.toCommandFromResource(resource);
-        var jwtResponse = userCommandService.handle(authenticateUserCommand);
+    public ResponseEntity<?> authenticateUser(@RequestBody SignInResource resource){
+        var signInCommand = SignInCommandFromResourceAssembler.toCommandFromResource(resource);
+        var authenticatedUser = userCommandService.handle(signInCommand);
+        if (authenticatedUser.isEmpty()) return ResponseEntity.notFound().build();
 
-        if (jwtResponse == null){
-            return ResponseEntity.badRequest().build();
-        }
-
-        var authenticatedResource = AuthenticationResourceFromAuthenticationResponseAssembler.toResourceFromResponse((JwtResponse) jwtResponse);
+        var authenticatedResource = AuthenticatedUserResourceFromEntityAssembler.toResourceFromEntity(authenticatedUser.get().getLeft(), authenticatedUser.get().getRight());
         return new ResponseEntity<>(authenticatedResource, HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody CreateUserResource resource){
-        var createUserCommand = CreateUserCommandFromResourceAssembler.toCommandFromResource(resource);
-        var userId = userCommandService.handle(createUserCommand);
+    public ResponseEntity<?> registerUser(@RequestBody SignUpResource resource){
+        var signInCommand = SignUpCommandFromResourceAssembler.toCommandFromResource(resource);
+        var userId = userCommandService.handle(signInCommand);
         if (userId == 0L){
             return ResponseEntity.badRequest().build();
         }
