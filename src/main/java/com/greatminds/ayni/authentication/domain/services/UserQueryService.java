e9d92c144b5ece9c63cd218a477ae3aad6311983
Caszarjos <zaratecastro2003@hotmail.com>
2023-11-17 00:35:05 -0500

    feat(User): Added new functions to commands and query services contract and implementations.

--- src/main/java/com/greatminds/ayni/authentication/domain/services/UserQueryService.java
+++ src/main/java/com/greatminds/ayni/authentication/domain/services/UserQueryService.java
@@ -2,14 +2,16 @@ package com.greatminds.ayni.authentication.domain.services;
 
 
 import com.greatminds.ayni.authentication.domain.model.aggregates.User;
+import com.greatminds.ayni.authentication.domain.model.queries.GetAllUsersQuery;
 import com.greatminds.ayni.authentication.domain.model.queries.GetUserByIdQuery;
 import com.greatminds.ayni.authentication.domain.model.queries.GetUserByUsernameQuery;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface UserQueryService {
     Optional<User> handle(GetUserByUsernameQuery query);
     Optional<User> handle(GetUserByIdQuery query);
-
+    List<User> handle(GetAllUsersQuery query);
 }
